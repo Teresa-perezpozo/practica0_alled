9e2f87d4d4374992aee89f2ad338680edec355e8
Teresa-perezpozo <t.perezdearenaza@alumnos.upm.es>
2025-10-02 13:42:21 +0200

    Initial commit

---
+++ src/practica0_alled/clase_practica0.java
@@ -0,0 +1,30 @@
+package practica0_alled;
+
+import java.util.Scanner;
+
+public class clase_practica0 {
+	public static void main(String[] args) {
+	//recibe un néro lo convirte en el umbral y le suma o resta 1 de 1 en 1
+	int umbral = 5;
+	
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Por favor escriba un entero:");
+	
+	int x = sc.nextInt();	
+	//con esto guardo el int que he pedido a la gente en el entero x 
+	if(x>umbral) 
+		System.out.println("por encima del umbral");
+	for(int i= x;i>umbral;i++){
+		x--;
+	}
+	if(x==5)
+		System.out.println("Has dado en el clavo!!");
+	else
+		System.out.println("por debajo de umbral");
+		for(int i= x;i<umbral;i++){
+			x++;
+		}
+	System.out.println(x);
+	}
+}
+
---
+++ src/practica0_alled/prueba_arrray.java
@@ -0,0 +1,17 @@
+package practica0_alled;
+
+public class prueba_arrray {
+
+	public static void main(String[] args) {
+		int[] a1= {1, 2, 3, 4};
+		int a2[] = {1, 2, 3, 4};
+		int a3[] = new int[4];
+		int x = 3;
+		int y =4;
+		System.out.println("a1==a2 "+ (a1==a2));
+		System.out.println("x==y "+(x==y));
+		System.out.println("longitud de a1 = " + a1.length);
+
+	}
+
+}
